3e56620747c4a524748d8a5292e2d1a4ac42de73
David <davidmanuel.raposeiras@rai.usc.es>
2025-04-06 11:56:08 +0200

    Final fixes v1.2

--- scripts/leaderboard.js
+++ scripts/leaderboard.js
@@ -1,3 +1,26 @@
+$(document).ready(() => {
+  const highlightColor = '#123D15';
+
+  $('#leaderboard-body').on('mouseenter', 'tr', function () {
+    $(this).css({
+      'background-color': highlightColor,
+      'transform': 'scale(1.02)',
+      'transition': 'all 0.2s ease',
+      'z-index': 1,
+    });
+  });
+
+  $('#leaderboard-body').on('mouseleave', 'tr', function () {
+    $(this).css({
+      'background-color': '',
+      'transform': 'scale(1)',
+      'transition': 'transform 0.2s ease, background-color 0.2s ease',
+      'z-index': '',
+    });
+  });
+});
+
+
 //APARTADO 4.3.1
 $(document).ready(() => {
     const tbody = $('#leaderboard-body');
@@ -29,6 +52,10 @@ $(document).ready(() => {
           setTimeout(() => {
             $('#leaderboard-body').append(row);
           }, index * 100); 
+
+          row.on('animationend', () => {
+            row.removeClass('animated-row');
+          });
         });
       },
       error: (xhr) => {
@@ -38,57 +65,10 @@ $(document).ready(() => {
     });
 });  
 
-$(document).ready(() => {
-    const highlightColor = '#123D15';
-  
-    // Delegación: se aplica a todos los tr incluso si se insertan más tarde
-    $('#leaderboard-body').on('mouseenter', 'tr', function () {
-      $(this).css({
-        'background-color': highlightColor,
-        'transform': 'scale(1.02)',
-        'transition': 'all 0.2s ease',
-        'z-index': 1,
-      });
-    });
-  
-    $('#leaderboard-body').on('mouseleave', 'tr', function () {
-      $(this).css({
-        'background-color': '',
-        'transform': 'scale(1)',
-        'transition': 'transform 0.2s ease, background-color 0.2s ease',
-        'z-index': '',
-      });
-    });
-  });
 
 
-  $(document).ready(() => {
-    let selectedRow = null;
+
   
-    $('#leaderboard-body').on('click', 'tr', function () {
-      const row = $(this);
-  
-      if (selectedRow && row.is(selectedRow)) {
-        // Si ya estaba seleccionada, desactivar centrado
-        $('html, body').animate({ scrollTop: 0 }, 500);
-        selectedRow.removeClass('focused-row');
-        selectedRow = null;
-      } else {
-        // Desactivar la fila anterior
-        if (selectedRow) {
-          selectedRow.removeClass('focused-row');
-        }
-  
-        // Activar la nueva fila
-        selectedRow = row;
-        row.addClass('focused-row');
-  
-        // Calcular posición y centrar
-        const offset = row.offset().top - ($(window).height() / 2) + (row.height() / 2);
-        $('html, body').animate({ scrollTop: offset }, 500);
-      }
-    });
-  });
   
 
 
